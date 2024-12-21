4b11b0d4cefc243605763c22f8bbe1f2546e84fb
Blair-Akuudo <emjayb24@gmail.com>
2024-12-21 13:07:10 +0100

    this is my version of the assignment

---
+++ BlessingJonah.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+who_am_i() {
+    echo "Hello, I am Blessing Jonah"
+}
+
+Bio() { 
+    echo "I am in the cloud engineering track with reg no: ALT/SOE/024/0918"
+    echo "I am 26years old, i'm a graduate of botany from the University of Lagos"
+    echo "I live in Lagos"
+    echo "I have background in cybersecurity"
+    echo "I love watching movies and sleeping" 
+}
+
+Years_in_tech() {
+    echo "I've been in tech for two year" 
+}
+
+Dream_job() {
+    echo "I would like to work as a cloud security architect with one of the Big 4" 
+}
+
+Goals() {
+    echo "I would love to become a professional in my field"
+    echo "To be sooo good that i get to retire by 38"
+    echo "To become an angel investor at some point and maybe own a start up"
+    echo "To improve on my programming languages" 
+}
+
+who_am_i 
+echo
+
+Bio 
+echo
+
+Years_in_tech 
+echo
+
+Dream_job 
+echo
+
+Goals 
+
